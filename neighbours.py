8e35aa546f93a740f5a533c8c55254bb55336f53
xinhe <u7518549@anu.edu.au>
2023-10-09 22:25:15 +1100

    2023-10-9 working on VNS frame

--- neighbours.py
+++ neighbours.py
@@ -1,3 +1,7 @@
+from typing import List
+import firefighter
+
+
 class Neighbourhood:
     """
     A neighbourhood is a class that computes explicitly neighbours of a schedule.
@@ -30,13 +34,41 @@ class SwapNeighbourhood(Neighbourhood):
                 ]  # notice that we can do that because strings are immutable (no risk of side effects)
                 new_schedule[i] = schedule[j]
                 new_schedule[j] = schedule[i]
-                # if not self._prob.is_feasible(new_schedule) == None:
-                #    continue
-                # No need to test for feasibility: this new schedule is guaranteed to be feasible (assuming the specified one was feasible).
+                # if not self._prob.is_feasible(new_schedule) == None: continue No need to test for feasibility: this
+                # new schedule is guaranteed to be feasible (assuming the specified one was feasible).
                 result.append(new_schedule)
         return result
 
 
 # TODO: Define more neighbourhoods, and remove this comment.
+class SwapWeekendNeighbourhood(Neighbourhood):
+    """
+    A neighbourhood that swaps the weekend schedules of a firefighter.
+    """
+
+    def __init__(self, prob):
+        super().__init__(prob)
+
+    def neighbours(self, schedule):
+        result = []
+        nb_weekend_days = self._prob._nb_weeks * 2  # Number of weekend days in the scheduling period
+
+        for i in range(self._prob._nb_firefighters):
+            # Create a copy of the current schedule
+            new_schedule = schedule[:]
+
+            for day in range(nb_weekend_days):
+                # Get the positions of the weekend days for the firefighter i
+                day_i = day
+
+                # Swap the schedules for the weekend days
+                new_schedule[i][day_i], new_schedule[i][day_i + nb_weekend_days] = new_schedule[i][
+                    day_i + nb_weekend_days], new_schedule[i][day_i]
+
+            # Check if the new schedule is feasible and add it to the result
+            if self._prob.is_feasible(new_schedule) is None:
+                result.append(new_schedule)
+
+        return result
 
 # eof
--- vns.py
+++ vns.py
@@ -1,5 +1,63 @@
-if __name__ == '__main__':
-    # TODO: Implement Variable Neighbourhood Search
-    pass
+import random
+import firefighter
+import neighbours
 
-# eof
+if __name__ == '__main__':
+    # Load the initial schedule from example.sched
+    initial_schedule = firefighter.load_schedule("example.sched")
+
+    prob = firefighter.SchedulingProblem()
+    costs = firefighter.read_costs(prob)
+
+    # Define neighbourhoods
+    neighborhoods = [neighbours.SwapNeighbourhood(prob)]
+
+    # Initialize current solution
+    current_solution = initial_schedule
+    current_cost = prob.cost(current_solution, costs)
+    print(f"initial cost is {current_cost}")
+
+    # Define your VNS parameters
+    max_iterations_without_improvement = 100  # Maximum number of iterations without improvement
+    kmax = len(neighborhoods)  # Number of neighborhoods
+
+    # Initiate VNS parameters
+    iterations_without_improvement = 0
+    k = 1
+
+    while k <= kmax:
+        # Choose the k-th neighbourhood
+        neighborhood = neighborhoods[k - 1]
+        print(type(neighborhood))
+
+        # Generate neighbors using the selected neighbourhood
+        neighbors = neighborhood.neighbours(current_solution)
+
+        # Evaluate the neighbors and find the best one
+        best_neighbor = None
+        best_neighbor_cost = current_cost
+
+        for neighbor in neighbors:
+            neighbor_cost = prob.cost(neighbor, costs)
+            if neighbor_cost < best_neighbor_cost:
+                best_neighbor = neighbor
+                best_neighbor_cost = neighbor_cost
+
+        # If a better neighbor is found, update the current solution and reset k
+        if best_neighbor is not None and best_neighbor_cost < current_cost:
+            current_solution = best_neighbor
+            current_cost = best_neighbor_cost
+            k = 1
+
+        else:
+            k += 1
+
+            # Increment iterations_without_improvement if no better neighbor is found
+            if best_neighbor is None or best_neighbor_cost >= current_cost:
+                iterations_without_improvement += 1
+    feasibility = prob.is_feasible(current_solution)
+    if feasibility:
+        # Save the final schedule
+        firefighter.save_schedule(current_solution)
+    else:
+        print("solution is infeasible")
