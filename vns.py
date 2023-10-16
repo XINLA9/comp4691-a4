775da8aca3b3bbbbed1bdd6f77436a34aecd3451
Xinhe <u7518549@anu.edu.au>
2023-10-16 14:38:54 +1100

    10-16 working on lns

--- vns.py
+++ vns.py
@@ -13,12 +13,11 @@ if __name__ == '__main__':
 
     # Define neighbourhoods list
     neighborhoods = [
-        # neighbours.SwapNeighbourhood(prob),
+        neighbours.SwapNeighbourhood(prob),
         neighbours.ChangOneDayNeighbourhood(prob),
         neighbours.OffDutyMoveNeighbourhood(prob),
         neighbours.SwapDaysNeighbourhood(prob),
         neighbours.TwoOffDutyMoveNeighbourhood(prob),
-
     ]
 
     # Initialize current solution
@@ -40,18 +39,18 @@ if __name__ == '__main__':
         neighbors = neighborhood.neighbours(current_solution)
 
         # Evaluate the neighbors and find the best improved one
-        best_neighbor = None
+        better_neighbor = None
         best_neighbor_cost = current_cost
 
         for neighbor in neighbors:
             neighbor_cost = prob.cost(neighbor, costs)
             if neighbor_cost < best_neighbor_cost:
-                best_neighbor = neighbor
+                better_neighbor = neighbor
                 best_neighbor_cost = neighbor_cost
 
         # If a better neighbor is found, update the current solution and reset k
-        if best_neighbor is not None and best_neighbor_cost < current_cost:
-            current_solution = best_neighbor
+        if better_neighbor is not None and best_neighbor_cost < current_cost:
+            current_solution = better_neighbor
             current_cost = best_neighbor_cost
             k = 1
             firefighter.save_schedule(current_solution)
